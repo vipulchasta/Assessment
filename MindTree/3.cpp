e252917e282aad2cf334fca805612dbbc442744f
Vipul Chasta <chasta.vipul@gmail.com>
2017-08-06 12:12:22 +0530

    Update
    Updating question and respective answer

--- MindTree/3.cpp
+++ MindTree/3.cpp
@@ -1,1 +1,34 @@
+/*
+    Swap Values with Index
+    Ex      [ 1 3 2 0]
+        ans [ 3 0 2 1]
+*/
 
+#include<iostream>
+
+using namespace std;
+
+int * myFunction( int *arr , int n )
+{
+    int *newArr = ( int * )malloc(n*sizeof(int));
+    
+    for( int index = 0 ; index < n ; index++ )
+    {
+        newArr[arr[index]] = index;
+    }
+    
+    return newArr;
+}
+
+int main()
+{
+    int testArr[] = { 1 , 3 , 2 , 0 };
+    int n = 4;
+    int *res = myFunction( testArr , n );
+    for( int index = 0 ; index < n ; index++ )
+    {
+        cout << res[index] << " ";
+    }
+    cout << endl;
+    return 0;
+}
